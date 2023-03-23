f337bd29417ef8067f67a3d77aaa37f2f0376ac6
LAITxNIKEYs <104034823+LAITxNIKEYs@users.noreply.github.com>
2023-03-23 09:24:57 +0300

    Update mainfun.js

--- mainfun.js
+++ mainfun.js
@@ -1,15 +1,16 @@
-
-var x,y,z;
-function randPoz() {
-  x = 0;
-  y =  Math.random(5) -5;
-  z = 0; 
-  return(x,y,z);
-}
-
-const gg = document.querySelector('#svin');
-function OnClik() {
-    gg.addEventListener('onclick', () => {
-       gg.setAttribute("position",`${0} ${y+1} ${0}` )
-    });
-}
+document.addEventListener("DOMContentLoaded", (event) => {
+    var x,y,z;
+    function randPoz() {
+      x = 0;
+      y =  Math.random(5) -5;
+      z = 0; 
+      return(x,y,z);
+    }
+    
+    const gg = document.querySelector('#svin');
+    function OnClik() {
+        gg.addEventListener('onclick', () => {
+            gg.setAttribute("position",`${0} ${y+1} ${0}` )
+        });
+    }
+});
